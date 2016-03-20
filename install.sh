7507f9abf18710e701d5e8a863e12287fc56269a
Garbaz <garbaz@t-online.de>
2016-03-20 21:09:27 +0100

    Added install script

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+targetfolder="/usr/include/garbaz/"
+echo "Will install netlib into the following folder:"
+echo "$targetfolder"
 ls /usr/include/garbaz/ 2>/dev/null 1>/dev/null
 if [ "$?" -ne "0" ];then
 sudo mkdir /usr/include/garbaz/
